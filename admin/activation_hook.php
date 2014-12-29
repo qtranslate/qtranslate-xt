6379961002bda85ed5196df075003590c3d9bbf2
johnclause <johnclause@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-29 17:35:25 +0000

    add new files
    git-svn-id: http://plugins.svn.wordpress.org/qtranslate-x/trunk@1056410 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/activation_hook.php
@@ -0,0 +1,150 @@
+<?php // encoding: utf-8
+/*
+	Copyright 2014  qTranslate Team  (email : qTranslateTeam@gmail.com )
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA02110-1301USA
+*/
+
+function qtranxf_admin_notice_deactivate_plugin($nm,$plugin)
+{
+	deactivate_plugins($plugin,true);
+	$d=dirname($plugin);
+	$link='<a href="https://wordpress.org/plugins/'.$d.'/" target="_blank">'.$nm.'</a>';
+	$qtxnm='qTranslate&#8209;X';
+	$qtxlink='<a href="https://wordpress.org/plugins/qtranslate-x/" target="_blank">'.$qtxnm.'</a>';
+	$f='qtranxf_migrate_import_'.str_replace('-','_',dirname($plugin));
+	$imported=false;
+	if(function_exists($f)){
+		global $wpdb;
+		$options = $wpdb->get_col("SELECT `option_name` FROM {$wpdb->options} WHERE `option_name` LIKE 'qtranslate_%'");
+		if(empty($options)){
+			$f();
+			$imported=true;
+		}
+	}
+	$msg=sprintf(__('Activation of plugin %s deactivated plugin %s since they cannot run simultaneously.','qtranslate'),$qtxlink,$link).' ';
+	if($imported){
+		$msg.=sprintf(__('The compatible settings from %s have been imported to %s. Further tuning, import, export and reset of options can be done at Settings/Languages configuration page, once %s is running.%sContinue%s','qtranslate'),$nm,$qtxnm,$qtxnm,'</p><p><a  class="button" href="">','</a>');
+	}else{
+		$msg.=sprintf(__('You may import/export compatible settings from %s to %s on Settings/Languages configuration page, once %s is running.%sContinue%s','qtranslate'),$nm,$qtxnm,$qtxnm,'</p><p><a  class="button" href="">','</a>');
+	}
+	//$nonce=wp_create_nonce('deactivate-plugin_'.$plugin);
+	//$msg=sprintf(__('Plugin %s cannot run concurrently with %s, please %sdeactivate %s%s. You may import compatible settings from %s to %s on Settings/Languages configuration page, once %s is running.','qtranslate'),$qtxlink,$link,'<a href="'.admin_url('plugins.php?action=deactivate&plugin='.encode($plugin).'&plugin_status=all&paged=1&s&_wpnonce='.$nonce.'">',$nm,'</a>',$nm,$qtxnm,$qtxnm);
+	//$msg=sprintf(__('Activation of plugin %s deactivated plugin %s since they cannot run simultaneously. You may import compatible settings from %s to %s on Settings/%sLanguages%s configuration page, once %s is running.%sContinue%s','qtranslate'),$qtxlink,$link,$nm,$qtxnm,'<a href="'.admin_url('/options-general.php?page=qtranslate-x').'">','</a>',$qtxnm,'</p><p><a  class="button" href="">','</a>');
+	wp_die('<p>'.$msg.'</p>');
+}
+
+function qtranxf_activation_hook()
+{
+	// Check if other qTranslate forks are activated.
+	if ( is_plugin_active( 'mqtranslate/mqtranslate.php' ) )
+		qtranxf_admin_notice_deactivate_plugin('mqTranslate','mqtranslate/mqtranslate.php');
+
+	if ( is_plugin_active( 'qtranslate/qtranslate.php' ) )
+		qtranxf_admin_notice_deactivate_plugin('qTranslate','qtranslate/qtranslate.php');
+
+	if ( is_plugin_active( 'qtranslate-xp/ppqtranslate.php' ) )
+		qtranxf_admin_notice_deactivate_plugin('qTranslate Plus','qtranslate-xp/ppqtranslate.php');
+
+	if ( is_plugin_active( 'ztranslate/ztranslate.php' ) )
+		qtranxf_admin_notice_deactivate_plugin('zTranslate','ztranslate/ztranslate.php');
+
+		//deactivate_plugins(plugin_basename(__FILE__)); // Deactivate ourself
+}
+
+/*
+function qtranxf_admin_notice_deactivated($plugin)
+{
+	$plugin_file=WP_CONTENT_DIR.'/plugins/'.$plugin;
+	$plugin_data=get_plugin_data( plugin_file, false, true );
+echo "qtranxf_admin_notice_deactivated: $plugin";
+var_dump($plugin_data);
+	if(!$plugin_data) return;
+	$nm='<a href="https://wordpress.org/plugins/'.dirname($plugin).'/">'.$plugin_data['Name'].'</a>';
+	echo printf(__('Plugin qTranslate&#8209;X deactivated plugin %s since they cannot run simultaneously. You may import compatible settings from %s to qTranslate&#8209;X on Settings/"<a href="%s">Languages</a>" configuration page.','qtranslate'),$nm,$nm,admin_url('options-general.php?page=qtranslate-x'));
+}
+
+function qtranxf_admin_notices($nm)
+{
+	//if($_SERVER['REQUEST_METHOD']!='GET') return;
+	if(isset($_REQUEST['qtx_dismiss'])){
+		update_option('qtranslate_admin_notices',array());
+		return;
+	}
+	$admin_notices=get_option('qtranslate_admin_notices',array());
+	if(empty($admin_notices)) return;
+	//echo '<div class="updated">';
+	echo '<div class="update-nag">';
+	echo '<div style="float: right"><a href="?qtx_dismiss"><small>dismiss</small></a></div>';
+	foreach($admin_notices as $key=>$notice){
+		echo '<p>';
+		switch($key){
+			case 'mqtranslate/mqtranslate.php':
+			case 'qtranslate/qtranslate.php':
+			case 'qtranslate-xp/ppqtranslate.php':
+			case 'ztranslate/ztranslate.php':
+				qtranxf_admin_notice_deactivated($key);
+				break;
+			default: echo $notice; break;
+		}
+		echo '</p>';
+	}
+	echo '</div>';
+}
+
+function qtranxf_check_qtranslate_other()
+{
+	// Check if other qTranslate forks are active.
+	$plugins=array();
+	if(is_plugin_active('mqtranslate/mqtranslate.php')) $plugins[]='qtranslate-xp/ppqtranslate.php';
+	if(is_plugin_active('qtranslate/qtranslate.php')) $plugins[]='qtranslate/qtranslate.php';
+	if(is_plugin_active('qtranslate-xp/ppqtranslate.php')) $plugins[]='qtranslate-xp/ppqtranslate.php';
+	if(is_plugin_active('ztranslate/ztranslate.php')) $plugins[]='ztranslate/ztranslate.php';
+	if(empty($plugins)) return;
+	$admin_notices=get_option('qtranslate_admin_notices',array());
+	$t=time();
+	foreach($plugins as $plugin){
+		$admin_notices[$plugin]=$t;
+	}
+	deactivate_plugins($plugins,true);
+	add_action('admin_notices', 'qtranxf_admin_notices');
+}
+//muplugins_loaded plugins_loaded
+//add_action('admin_init', 'qtranxf_check_qtranslate_other', 0);
+*/
+
+function qtranxf_admin_notice_plugin_conflict($title,$plugin)
+{
+	if(!is_plugin_active($plugin)) return;
+	$me='<a href="https://wordpress.org/plugins/qtranslate-x/" style="color:blue" target="_blank">qTranslate&#8209;X</a>';
+	$link='<a href="https://wordpress.org/plugins/'.dirname($plugin).'/" style="color:magenta" target="_blank">'.$title.'</a>';
+	echo '<div class="error"><p style="font-size: larger">';
+	echo '<span style="color:red"><strong>'.__('Error').':</strong></span> '.sprintf(__('plugin %s cannot run concurrently with plugin %s. You may import and export compatible settings between %s and %s on Settings/<a href="%s">Languages</a> configuration page. Then you have to deactivate one of the plugins to continue.','qtranslate'),$me,$link,'qTranslate&#8209;X',$title,admin_url('options-general.php?page=qtranslate-x'));
+	$nonce=wp_create_nonce('deactivate-plugin_'.$plugin);
+	echo '</p><p> &nbsp; &nbsp; &nbsp; &nbsp;<a class="button" href="'.admin_url('plugins.php?action=deactivate&plugin='.urlencode($plugin).'&plugin_status=all&paged=1&s&_wpnonce='.$nonce).'"><strong>'.__('Deactivate ').'<span style="color:magenta">'.$title.'</span></strong></a>';
+	$nonce=wp_create_nonce('deactivate-plugin_qtranslate-x/qtranslate.php');
+	echo ' &nbsp; &nbsp; &nbsp; &nbsp;<a class="button" href="'.admin_url('plugins.php?action=deactivate&plugin='.urlencode('qtranslate-x/qtranslate.php').'&plugin_status=all&paged=1&s&_wpnonce='.$nonce).'"><strong>'.__('Deactivate ').'<span style="color:blue">qTranslate&#8209;X</span></strong></a>';
+	echo '</p></div>';
+}
+
+function qtranxf_admin_notices_plugin_conflicts()
+{
+	qtranxf_admin_notice_plugin_conflict('qTranslate','qtranslate/qtranslate.php');
+	qtranxf_admin_notice_plugin_conflict('mqTranslate','mqtranslate/mqtranslate.php');
+	qtranxf_admin_notice_plugin_conflict('qTranslate Plus','qtranslate-xp/ppqtranslate.php');
+	qtranxf_admin_notice_plugin_conflict('zTranslate','ztranslate/ztranslate.php');
+}
+add_action('admin_notices', 'qtranxf_admin_notices_plugin_conflicts');
+?>
---
+++ admin/import_export.php
@@ -0,0 +1,131 @@
+<?php // encoding: utf-8
+/*
+	Copyright 2014  qTranslate Team  (email : qTranslateTeam@gmail.com )
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA02110-1301USA
+*/
+
+function qtranxf_migrate_options_update($nm_to,$nm_from)
+{
+	global $wpdb;
+	$option_names = $wpdb->get_col("SELECT `option_name` FROM {$wpdb->options} WHERE `option_name` LIKE '$nm_to\_%'");
+	foreach ($option_names as $name)
+	{
+		if(strpos($name,'_flag_location')>0) continue;
+		$nm = str_replace($nm_to,$nm_from,$name);
+		$value=get_option($nm);
+		if($value===FALSE) continue;
+		update_option($name,$value);
+	}
+}
+
+function qtranxf_migrate_options_copy($nm_to,$nm_from)
+{
+	global $wpdb;
+	$options = $wpdb->get_results("SELECT option_name, option_value FROM {$wpdb->options} WHERE `option_name` LIKE '$nm_from\_%'");
+	foreach ($options as $option)
+	{
+		$name=$option->option_name;
+		$value=$option->option_value;
+		if(strpos($name,'_flag_location')>0) continue;
+		$nm = str_replace($nm_from,$nm_to,$name);
+		update_option($nm,$value);
+	}
+}
+
+function qtranxf_migrate_import_mqtranslate(){ qtranxf_migrate_options_update('qtranslate','mqtranslate'); }
+function qtranxf_migrate_export_mqtranslate(){ qtranxf_migrate_options_copy('mqtranslate','qtranslate'); }
+
+function qtranxf_migrate_import_qtranslate_xp(){ qtranxf_migrate_options_update('qtranslate','ppqtranslate'); }
+function qtranxf_migrate_export_qtranslate_xp(){ qtranxf_migrate_options_copy('ppqtranslate','qtranslate'); }
+
+//function qtranxf_migrate_import_ztranslate(){ qtranxf_migrate_options_update('qtranslate','ztranslate'); }
+//function qtranxf_migrate_export_ztranslate(){ qtranxf_migrate_options_copy('ztranslate','qtranslate'); }
+
+function qtranxf_migrate_plugin($plugin){
+	$var=$plugin.'-migration';
+	if(!isset($_POST[$var])) return;
+	if($_POST[$var]=='none') return;
+	qtranxf_loadConfig();
+	qtranxf_saveConfig();
+	$f='qtranxf_migrate_'.$_POST[$var].'_'.str_replace('-','_',$plugin);
+	$f();
+}
+
+function qtranxf_migrate_plugins()
+{
+	qtranxf_migrate_plugin('mqtranslate');
+	qtranxf_migrate_plugin('qtranslate-xp');
+	//qtranxf_migrate_plugin('ztranslate');//ok same db
+}
+add_action('qtranxf_init_begin','qtranxf_migrate_plugins',11);
+
+function qtranxf_add_row_migrate($nm,$plugin) {
+	if(!file_exists(WP_CONTENT_DIR.'/plugins/'.$plugin)) return;
+?>
+<tr valign="top" id="qtranslate-<?php echo $plugin; ?>">
+	<th scope="row"><?php _e('Plugin');?> <a href="https://wordpress.org/plugins/<?php echo $plugin; ?>/" target="_blank"><?php echo $nm; ?></a></th>
+	<td>
+<?php
+	if($plugin=='qtranslate' || $plugin=='ztranslate'){
+		_e('There is no need to migrate any setting, the database schema is compatible with this plugin.');
+	}else{
+?>
+		<label for="qtranslate_no_migration"><input type="radio" name="<?php echo $plugin; ?>-migration" id="<?php echo $plugin; ?>_no_migration" value="none" checked /> <?php _e('Do not migrate any setting', 'qtranslate'); ?></label>
+		<br/>
+		<label for="qtranslate_import_migration"><input type="radio" name="<?php echo $plugin; ?>-migration" id="qtranslate_import_migration" value="import" /> <?php echo __('Import settings from ', 'qtranslate').$nm; ?></label>
+		<br/>
+		<label for="qtranslate_export_migration"><input type="radio" name="<?php echo $plugin; ?>-migration" id="qtranslate_export_migration" value="export" /> <?php echo __('Export settings to ', 'qtranslate').$nm; ?></label>
+<?php } ?>
+	</td>
+</tr>
+<?php
+}
+
+function qtranxf_admin_section_import_export($request_uri)
+{
+	qtranxf_admin_section_start(__('Import').'/'.__('Export'),'import');
+?>
+	<table class="form-table" id="qtranslate-admin-import" style="display: none">
+		<tr valign="top" id="qtranslate-convert-database">
+			<th scope="row"><?php _e('Convert Database', 'qtranslate');?></th>
+			<td>
+				<?php printf(__('If you are updating from qTranslate 1.x or Polyglot, <a href="%s">click here</a> to convert posts to the new language tag format.', 'qtranslate'), $clean_uri.'&convert=true'); ?>
+				<?php printf(__('If you have installed qTranslate for the first time on a Wordpress with existing posts, you can either go through all your posts manually and save them in the correct language or <a href="%s">click here</a> to mark all existing posts as written in the default language.', 'qtranslate'), $clean_uri.'&markdefault=true'); ?>
+				<?php _e('Both processes are <b>irreversible</b>! Be sure to make a full database backup before clicking one of the links.', 'qtranslate'); ?>
+			</td>
+		</tr>
+		<?php qtranxf_add_row_migrate('qTranslate','qtranslate'); ?>
+		<?php qtranxf_add_row_migrate('mqTranslate','mqtranslate'); ?>
+		<?php qtranxf_add_row_migrate('qTranslate Plus','qtranslate-xp'); ?>
+		<?php qtranxf_add_row_migrate('zTranslate','ztranslate'); ?>
+		<tr valign="top">
+			<th scope="row"><?php _e('Reset qTranslate', 'qtranslate');?></th>
+			<td>
+				<label for="qtranslate_reset"><input type="checkbox" name="qtranslate_reset" id="qtranslate_reset" value="1"/> <?php _e('Check this box and click Save Changes to reset all qTranslate settings.', 'qtranslate'); ?></label>
+				<br/>
+				<label for="qtranslate_reset2"><input type="checkbox" name="qtranslate_reset2" id="qtranslate_reset2" value="1"/> <?php _e('Yes, I really want to reset qTranslate.', 'qtranslate'); ?></label>
+				<br/>
+				<label for="qtranslate_reset3"><input type="checkbox" name="qtranslate_reset3" id="qtranslate_reset3" value="1"/> <?php _e('Also delete Translations for Categories/Tags/Link Categories.', 'qtranslate'); ?></label>
+				<br/>
+				<?php _e('If something isn\'t working correctly, you can always try to reset all qTranslate settings. A Reset won\'t delete any posts but will remove all settings (including all languages added).', 'qtranslate'); ?>
+			</td>
+		</tr>
+	</table>
+<?php
+	qtranxf_admin_section_end('import');
+}
+add_action('qtranslate_configuration', 'qtranxf_admin_section_import_export', 9);
+?>
