b88ecefc2b7ea7af97ce9d484e323a11b5a77ae0
johnclause <johnclause@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-03 19:14:21 +0000

    former function names
    git-svn-id: http://plugins.svn.wordpress.org/qtranslate-x/trunk@1059163 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ qtranslate_compatibility.php
@@ -0,0 +1,29 @@
+<?php
+if(!function_exists('qtrans_convertURL')){
+	function qtrans_convertURL($url='', $lang='', $forceadmin = false, $showDefaultLanguage = false){
+		return qtranxf_convertURL($url, $lang, $forceadmin, $showDefaultLanguage);
+	}
+}
+if(!function_exists('qtrans_getLanguage')){
+	function qtrans_getLanguage(){
+		return qtranxf_getLanguage();
+	}
+}
+
+if(!function_exists('qtrans_use')){
+	function qtrans_use($lang, $text, $show_available=false){
+		return qtranxf_use($lang, $text, $show_available);
+	}
+}
+
+if (function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage')){
+	function qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($content){
+		return qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage($content);
+	}
+}
+
+//if(!function_exists('qtrans_useTermLib')){//is not available any more
+//	function qtrans_useTermLib($obj){ return qtranxf_useTermLib($obj);
+//}
+
+?>
