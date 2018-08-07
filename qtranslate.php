241abdd4748f07f2360c12f32cd69321a3677f0c
HerrVigg <herrvigg@gmail.com>
2018-08-07 17:02:03 +0200

    move experimental slugs to dev, export-ignore dev files

--- qtranslate.php
+++ qtranslate.php
@@ -71,8 +71,8 @@ if(is_admin() ){ // && !(defined('DOING_AJAX') && DOING_AJAX) //todo cleanup
 
 // load additional functionalities
 
-//if(file_exists(QTRANSLATE_DIR.'/slugs'))
-//	require_once(QTRANSLATE_DIR.'/slugs/qtx_slug.php');
+//if(file_exists(QTRANSLATE_DIR.'/dev/slugs'))
+//	require_once(QTRANSLATE_DIR.'/dev/slugs/qtx_slug.php');
 
 // load qTranslate Services if available // disabled since 3.1
 //if(file_exists(QTRANSLATE_DIR.'/qtranslate_services.php'))
