6f731931c860c79b0ff53aba9980d17854784286
HerrVigg <herrvigg@gmail.com>
2019-06-23 12:27:32 +0200

    Fix deprecated WC filter in order-received (#705)
    Deprecated in WC 3.0.0: woocommerce_order_items_table
    Replaced with: woocommerce_order_details_after_order_table_items
    
    Also done some cleanup and formatting.

--- modules/woo-commerce/qwc-front.php
+++ modules/woo-commerce/qwc-front.php
@@ -9,52 +9,36 @@ function qwc_add_filters_front() {
 	add_filter( 'get_post_metadata', 'qwc_filter_postmeta', 5, 4 );
 
 	$use_filters = array(
-		/* do not exist any more */
-		//'option_woocommerce_email_from_name' => 10,
-		//'the_title_attribute' => 10,
-		//'woocommerce_order_product_title' => 10,
-		//'woocommerce_composite_component_title' => 10,
-		//'woocommerce_composite_component_description' => 10,
-		//'woocommerce_composited_product_excerpt' => 10,
-		//'woocommerce_bto_component_title' => 10,
-		//'woocommerce_bto_component_description' => 10,
-		//'woocommerce_bto_product_excerpt' => 10,
-		//'woocommerce_bundled_item_title' => 10,
-		//'woocommerce_bundled_item_description' => 10,
-		//'woocommerce_in_cart_product_title' => 10,
-		//'woocommerce_order_table_product_title' => 10,
-
 		/* one-argument filters */
-		'wp_mail_from_name'                           => 20,
-		'woocommerce_order_item_display_meta_value'   => 20,
-		'woocommerce_order_items_table'               => 20,
-		'woocommerce_page_title'                      => 20,
-		'woocommerce_short_description'               => 20,
-		'woocommerce_variation_option_name'           => 20,
-		//'woocommerce_shipping_methods' => 20
+		'wp_mail_from_name'                                 => 20,
+		'woocommerce_order_item_display_meta_value'         => 20,
+		'woocommerce_order_details_after_order_table_items' => 20,
+		'woocommerce_page_title'                            => 20,
+		'woocommerce_short_description'                     => 20,
+		'woocommerce_variation_option_name'                 => 20,
 
 		/* two-argument filters */
-		'woocommerce_attribute_label'                 => 20,
-		'woocommerce_cart_shipping_method_full_label' => 20,
-		'woocommerce_cart_tax_totals'                 => 20,
-		'woocommerce_email_footer_text'               => 20,
-		'woocommerce_gateway_description'             => 20,
-		'woocommerce_gateway_title'                   => 20,
-		'woocommerce_gateway_icon'                    => 20,
-		'woocommerce_order_item_name'                 => 20,
-		'woocommerce_order_shipping_to_display'       => 20,
-		'woocommerce_order_get_tax_totals'            => 20,
-		'woocommerce_product_title'                   => 20,
-		'woocommerce_rate_label'                      => 20,
+		'woocommerce_attribute_label'                       => 20,
+		'woocommerce_cart_shipping_method_full_label'       => 20,
+		'woocommerce_cart_tax_totals'                       => 20,
+		'woocommerce_email_footer_text'                     => 20,
+		'woocommerce_gateway_description'                   => 20,
+		'woocommerce_gateway_title'                         => 20,
+		'woocommerce_gateway_icon'                          => 20,
+		'woocommerce_order_item_name'                       => 20,
+		'woocommerce_order_shipping_to_display'             => 20,
+		'woocommerce_order_get_tax_totals'                  => 20,
+		'woocommerce_product_title'                         => 20,
+		'woocommerce_rate_label'                            => 20,
 
 		/* three-argument filters */
-		'woocommerce_attribute'                       => 20,
-		'woocommerce_cart_item_name'                  => 20,
-		'woocommerce_cart_item_thumbnail'             => 20,
-		'woocommerce_order_subtotal_to_display'       => 20,
+		'woocommerce_attribute'                             => 20,
+		'woocommerce_cart_item_name'                        => 20,
+		'woocommerce_cart_item_thumbnail'                   => 20,
+		'woocommerce_order_subtotal_to_display'             => 20,
 
 		/* four-argument filters */
-		'woocommerce_format_content'                  => 20,
+		'woocommerce_format_content'                        => 20,
 		//function wc_format_content in woocommerce/includes/wc-formatting-functions.php
 
 		//not in front
