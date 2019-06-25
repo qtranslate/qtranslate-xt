6cb3968a2e28136f2f5446991dad45757d42afe1
HerrVigg <herrvigg@gmail.com>
2019-06-26 00:59:36 +0200

    Fix Jetpack: translate related post titles (#699)

--- modules/jetpack/jetpack.php
+++ modules/jetpack/jetpack.php
@@ -8,7 +8,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class QTX_Jetpack {
 	public function __construct() {
+		add_filter( 'jetpack_relatedposts_returned_results', array( $this, 'translate_related_posts' ) );
 	}
+
+	/**
+	 * Translates related posts through Jetpack REST API
+	 *
+	 * @param array $results related posts with fetched data in Jetpack format
+	 *
+	 * @return array updated related posts
+	 * @see get_related_post_data_for_post in jetpack/modules/related_posts/jetpack-related-posts.php
+	 */
+	function translate_related_posts( $results ) {
+		return array_map( function ( $result ) {
+			$result['title']   = qtranxf_useCurrentLanguageIfNotFoundShowAvailable( $result['title'] );
+			$result['excerpt'] = qtranxf_useCurrentLanguageIfNotFoundShowAvailable( $result['excerpt'] );
+
+			return $result;
+		}, $results );
+	}
+
 }
 
 new QTX_Jetpack();
