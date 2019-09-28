433fc83ba03e5dafb9db0e1b772571901f36e344
HerrVigg <herrvigg@gmail.com>
2019-09-28 21:54:16 +0200

    Remove obsolete qtranslate_custom_admin_js in ACF

--- modules/acf/src/qtranslatex.php
+++ modules/acf/src/qtranslatex.php
@@ -29,7 +29,6 @@ class acf_qtranslate_qtranslatex {
 		require_once ACF_QTRANSLATE_PLUGIN_DIR . 'compatibility/qtranslatex.php';
 
 		add_action('admin_head',                         array($this, 'admin_head'));
-		add_filter('qtranslate_custom_admin_js',         array($this, 'qtranslate_custom_admin_js'));
 		add_filter('acf_qtranslate_get_active_language', array($this, 'get_active_language'));
 		add_action('acf/input/admin_enqueue_scripts',    array($this, 'admin_enqueue_scripts'));
 	}
@@ -66,18 +65,6 @@ class acf_qtranslate_qtranslatex {
 	public function admin_enqueue_scripts() {
 		$version = $this->plugin->acf_major_version();
 		wp_enqueue_script('acf_qtranslatex', plugins_url("/assets/acf_{$version}/qtranslatex.js",  ACF_QTRANSLATE_PLUGIN), array('acf_qtranslate_common'));
-	}
-
-	/**
-	 * Use the edit-post script on admin pages.
-	 * @return string
-	 */
-	public function qtranslate_custom_admin_js() {
-		global $pagenow, $plugin_page;
-
-		if ($pagenow === 'admin.php' && isset($plugin_page)) {
-			return 'admin/js/edit-post';
-		}
 	}
 
 	/**
