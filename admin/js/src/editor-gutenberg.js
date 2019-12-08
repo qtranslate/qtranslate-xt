1970032185b0e49110e4651941269759c3de0f2a
HerrVigg <herrvigg@gmail.com>
2019-12-08 21:29:20 +0100

    Extend js handler to any post type

--- admin/js/src/editor-gutenberg.js
+++ admin/js/src/editor-gutenberg.js
@@ -6,28 +6,42 @@
 
 (function () {
     console.log('QT-XT API: setup apiFetch');
-
     wp.apiFetch.use((options, next) => {
-        if (options.path) {
-            const post = wp.data.select('core/editor').getCurrentPost();
-            if ((options.path.startsWith('/wp/v2/posts/' + post.id) && options.method == 'PUT') ||
-                (options.path.startsWith('/wp/v2/posts/' + post.id + '/autosaves') && options.method == 'POST')) {
-                console.log('QT-XT API: handling method=' + options.method, 'path=' + options.path, 'post=', post);
-                if (! post.hasOwnProperty('qtx_editor_lang')) {
-                    console.log('QT-XT API: missing field [qtx_editor_lang] in post id=' + post.id);
-                    return next(options);
-                }
-                const newOptions = {
-                    ...options,
-                    data: {
-                        ...options.data,
-                        'qtx_editor_lang': post.qtx_editor_lang
-                    }
-                };
-                console.log('QT-XT API: using options=', options);
-                const result = next(newOptions);
-                return result;
+        if (!options.path || (options.method !== 'PUT' && options.method !== 'POST')) {
+            return next(options);
+        }
+
+        // A better event handler is needed to understand when the post is saved.
+        // For now "wait" by ignoring all API calls until the post is loaded in the editor.
+        const post = wp.data.select('core/editor').getCurrentPost();
+        // console.log('QT-XT API: PRE handling method=' + options.method, 'path=' + options.path, 'post=', post);
+        if (!post.hasOwnProperty('type')) {
+            return next(options);
+        }
+        const typeData = wp.data.select('core').getPostType(post.type);
+        if (!typeData.hasOwnProperty('rest_base')) {
+            return next(options);
+        }
+        //console.log('QT-XT API: PRE handling method=' + options.method, 'path=' + options.path, 'post=', post, 'type=', typeData);
+        const prefixPath = '/wp/v2/' + typeData.rest_base + '/' + post.id;
+
+        if ((options.path.startsWith(prefixPath) && options.method === 'PUT') ||
+            (options.path.startsWith(prefixPath + '/autosaves') && options.method === 'POST')) {
+            console.log('QT-XT API: handling method=' + options.method, 'path=' + options.path, 'post=', post);
+            if (!post.hasOwnProperty('qtx_editor_lang')) {
+                console.log('QT-XT API: missing field [qtx_editor_lang] in post id=' + post.id);
+                return next(options);
             }
+            const newOptions = {
+                ...options,
+                data: {
+                    ...options.data,
+                    'qtx_editor_lang': post.qtx_editor_lang
+                }
+            };
+            console.log('QT-XT API: using options=', options);
+            const result = next(newOptions);
+            return result;
         }
         return next(options);
     });
