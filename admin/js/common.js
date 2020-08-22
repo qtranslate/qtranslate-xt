b89e1db618e251f88eaef4f363ccdd5bdbfb0b3a
HerrVigg <herrvigg@gmail.com>
2020-08-22 23:16:09 +0200

    Remove obsolete xsplit from String.prototype
    All browsers should now support String.split.
    It was even available in IE4...

--- admin/js/common.js
+++ admin/js/common.js
@@ -32,8 +32,8 @@ var qTranslateConfig = window.qTranslateConfig;
  * since 3.2.7
  */
 qtranxj_get_split_blocks = function (text) {
-    var split_regex = /(<!--:[a-z]{2}-->|<!--:-->|\[:[a-z]{2}]|\[:]|{:[a-z]{2}}|{:})/gi; // @since 3.3.6 swirly brackets
-    return text.xsplit(split_regex);
+    var split_regex = /(<!--:[a-z]{2}-->|<!--:-->|\[:[a-z]{2}]|\[:]|{:[a-z]{2}}|{:})/gi;
+    return text.split(split_regex);
 };
 
 /**
@@ -105,33 +105,6 @@ qtranxj_split_blocks = function (blocks) {
         }
     }
     return result;
-};
-
-String.prototype.xsplit = function (_regEx) {
-    // Most browsers can do this properly, so let them work, they'll do it faster
-    if ('a~b'.split(/(~)/).length === 3) {
-        return this.split(_regEx);
-    }
-
-    if (!_regEx.global) {
-        _regEx = new RegExp(_regEx.source, 'g' + (_regEx.ignoreCase ? 'i' : ''));
-    }
-
-    // IE (and any other browser that can't capture the delimiter)
-    // will, unfortunately, have to be slowed down
-    var start = 0, arr = [];
-    var result;
-    while ((result = _regEx.exec(this)) != null) {
-        arr.push(this.slice(start, result.index));
-        if (result.length > 1)
-            arr.push(result[1]);
-        start = _regEx.lastIndex;
-    }
-    if (start < this.length)
-        arr.push(this.slice(start));
-    if (start === this.length)
-        arr.push(''); // delim at the end
-    return arr;
 };
 
 function qtranxj_ce(tagName, props, pNode, isFirst) {
