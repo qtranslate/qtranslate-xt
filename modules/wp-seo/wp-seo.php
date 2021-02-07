c73b22fe979c1c4c86d6f615d5b779d5c69d10fa
HerrVigg <herrvigg@gmail.com>
2021-02-08 00:29:29 +0100

    [yoast] Prefix functions with qtranxf_wpseo (#947)

--- modules/wp-seo/wp-seo.php
+++ modules/wp-seo/wp-seo.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-function qwpseo_init_language( $url_info ) {
+function qtranxf_wpseo_init_language( $url_info ) {
     if ( $url_info['doing_front_end'] ) {
         require_once( dirname( __FILE__ ) . '/wp-seo-front.php' );
     } else {
@@ -13,4 +13,4 @@ function qwpseo_init_language( $url_info ) {
     }
 }
 
-add_action( 'qtranslate_init_language', 'qwpseo_init_language' );
+add_action( 'qtranslate_init_language', 'qtranxf_wpseo_init_language' );
