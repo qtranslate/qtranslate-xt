af881940f0ef9647b52ffb11486c098abc6be4e6
HerrVigg <herrvigg@gmail.com>
2021-02-07 23:21:48 +0100

    Yoast: translate org/company name (#947)

--- modules/wp-seo/wp-seo-admin.php
+++ modules/wp-seo/wp-seo-admin.php
@@ -2,3 +2,22 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
+
+add_filter( 'qtranslate_load_admin_page_config', 'qtranxf_wpseo_load_admin_page_config' );
+function qtranxf_wpseo_load_admin_page_config( $page_configs ) {
+    assert( ! isset( $page_configs['yoast_wpseo'] ) );
+
+    $page_configs['yoast_wpseo'] = array(
+        'pages' => array( 'admin.php' => 'wpseo_titles' ),
+        'forms' => array(
+            array(
+                'form'   => array( 'wpseo-conf' ),
+                'fields' => array(
+                    array( 'id' => 'company_name' ),
+                )
+            )
+        )
+    );
+
+    return $page_configs;
+}
