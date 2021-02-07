b21b745e547befb2ca5cc30e49be8c4cde99a5ff
HerrVigg <herrvigg@gmail.com>
2021-02-08 00:26:40 +0100

    [yoast] Generalize front filters (#947)

--- modules/wp-seo/wp-seo-front.php
+++ modules/wp-seo/wp-seo-front.php
@@ -3,4 +3,15 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-add_filter('wpseo_opengraph_title', 'qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage');
+function qtranxf_wpseo_add_filters_front() {
+    $use_filters = array(
+        'wpseo_opengraph_title' => 20,
+    );
+
+    foreach ( $use_filters as $name => $priority ) {
+        add_filter( $name, 'qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage', $priority );
+    }
+}
+
+// TODO: trigger this with a proper action - i18_front_config can't be used in modules, here it's too late!
+qtranxf_wpseo_add_filters_front();
