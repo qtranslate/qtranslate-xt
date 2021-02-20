591fb09c426570d1e0fb216d76827671f264ee37
HerrVigg <herrvigg@gmail.com>
2021-02-20 12:09:31 +0100

    [Yoast] Add front filters schema and breadcumbs (#947)

--- modules/wp-seo/wp-seo-front.php
+++ modules/wp-seo/wp-seo-front.php
@@ -20,6 +20,22 @@ function qtranxf_wpseo_add_filters_front() {
     foreach ( $use_filters as $name => $priority ) {
         add_filter( $name, 'qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage', $priority );
     }
+
+    add_filter( 'wpseo_schema_webpage', 'qtranxf_wpseo_webpage_schema', 10, 2 );
+    function qtranxf_wpseo_webpage_schema( $piece, $context ) {
+        if ( array_key_exists( 'description', $piece ) ) {
+            $piece['description'] = qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage( $piece['description'] );
+        }
+
+        return $piece;
+    }
+
+    add_filter( 'wpseo_breadcrumb_single_link_info', 'qtranxf_wpseo_breadcrumbs_link', 10, 3 );
+    function qtranxf_wpseo_breadcrumbs_link( $link_info, $index, $crumbs ) {
+        $link_info['text'] = qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage( $link_info['text'] );
+
+        return $link_info;
+    }
 }
 
 // TODO: trigger this with a proper hook - i18n_front_config can't be used in modules, here it's too late!
