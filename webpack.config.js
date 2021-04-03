60fafdbf1bbdbb6f6bf86fc3e8ef2e4718203b45
HerrVigg <herrvigg@gmail.com>
2021-04-03 19:05:32 +0200

    Remove unnecessary webpack options

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,6 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "[name].js"
-    },
-    externals: {
-        jquery: 'jQuery'
     },
     module: {
         rules: [
