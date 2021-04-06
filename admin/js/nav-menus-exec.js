a957fac08d5b3550a380604ec5bed5832f4bff9a
HerrVigg <herrvigg@gmail.com>
2021-04-06 19:21:39 +0200

    Refactor jQuery wrapper with local const

--- admin/js/nav-menus-exec.js
+++ admin/js/nav-menus-exec.js
@@ -1,56 +1,56 @@
 /* executed for
  /wp-admin/nav-menus.php
 */
-(function ($) {
-    $(function () {
-        const qtx = qTranslateConfig.js.get_qtx();
+const $ = jQuery;
 
-        const addMenuItemHooks = function (li) {
-            qtx.addContentHooksByClass('edit-menu-item-title', li);
-            qtx.addContentHooksByClass('edit-menu-item-attr-title', li);
-            qtx.addContentHooksByClass('[edit-menu-item-description', li); // must use '[:]' separator style
+$(function () {
+    const qtx = qTranslateConfig.js.get_qtx();
 
-            qtx.addDisplayHooksByClass('menu-item-title', li);
-            qtx.addDisplayHooksByTagInClass('link-to-original', 'A', li);
-        };
+    const addMenuItemHooks = function (li) {
+        qtx.addContentHooksByClass('edit-menu-item-title', li);
+        qtx.addContentHooksByClass('edit-menu-item-attr-title', li);
+        qtx.addContentHooksByClass('[edit-menu-item-description', li); // must use '[:]' separator style
 
-        const onAddMenuItem = function (menuMarkup) {
-            const rx = /id="menu-item-(\d+)"/gi;
-            while ((matches = rx.exec(menuMarkup))) {
-                const id = 'menu-item-' + matches[1];
-                const li = document.getElementById(id);
-                if (li) addMenuItemHooks(li);
-            }
-        };
+        qtx.addDisplayHooksByClass('menu-item-title', li);
+        qtx.addDisplayHooksByTagInClass('link-to-original', 'A', li);
+    };
 
+    const onAddMenuItem = function (menuMarkup) {
+        const rx = /id="menu-item-(\d+)"/gi;
+        while ((matches = rx.exec(menuMarkup))) {
+            const id = 'menu-item-' + matches[1];
+            const li = document.getElementById(id);
+            if (li) addMenuItemHooks(li);
+        }
+    };
+
+    if (wpNavMenu) {
+        const wp_addMenuItemToBottom = wpNavMenu.addMenuItemToBottom;
+        if (typeof wp_addMenuItemToBottom == 'function') {
+            wpNavMenu.addMenuItemToBottom = function (menuMarkup, req) {
+                wp_addMenuItemToBottom(menuMarkup, req);
+                onAddMenuItem(menuMarkup);
+            };
+        }
+        if (typeof wp_addMenuItemToTop == 'function') {
+            wpNavMenu.addMenuItemToTop = function (menuMarkup) {
+                wp_addMenuItemToTop(menuMarkup);
+                onAddMenuItem(menuMarkup);
+            };
+        }
+    }
+
+    const onLanguageSwitchAfter = function (lang) {
         if (wpNavMenu) {
-            const wp_addMenuItemToBottom = wpNavMenu.addMenuItemToBottom;
-            if (typeof wp_addMenuItemToBottom == 'function') {
-                wpNavMenu.addMenuItemToBottom = function (menuMarkup, req) {
-                    wp_addMenuItemToBottom(menuMarkup, req);
-                    onAddMenuItem(menuMarkup);
-                };
+            if (typeof wpNavMenu.refreshKeyboardAccessibility == 'function') {
+                wpNavMenu.refreshKeyboardAccessibility();
             }
-            if (typeof wp_addMenuItemToTop == 'function') {
-                wpNavMenu.addMenuItemToTop = function (menuMarkup) {
-                    wp_addMenuItemToTop(menuMarkup);
-                    onAddMenuItem(menuMarkup);
-                };
+            if (typeof wpNavMenu.refreshAdvancedAccessibility == 'function') {
+                wpNavMenu.refreshAdvancedAccessibility();
             }
         }
+    };
+    onLanguageSwitchAfter();
 
-        const onLanguageSwitchAfter = function (lang) {
-            if (wpNavMenu) {
-                if (typeof wpNavMenu.refreshKeyboardAccessibility == 'function') {
-                    wpNavMenu.refreshKeyboardAccessibility();
-                }
-                if (typeof wpNavMenu.refreshAdvancedAccessibility == 'function') {
-                    wpNavMenu.refreshAdvancedAccessibility();
-                }
-            }
-        };
-        onLanguageSwitchAfter();
-
-        qtx.addLanguageSwitchAfterListener(onLanguageSwitchAfter);
-    });
-})(jQuery);
+    qtx.addLanguageSwitchAfterListener(onLanguageSwitchAfter);
+});
--- admin/js/options.js
+++ admin/js/options.js
@@ -1,106 +1,106 @@
 /* executed for
  /wp-admin/options-general.php
 */
-(function ($) {
+const $ = jQuery;
 
-    const keySection = 'qtranslate-xt-admin-section';
-    const switchTab = function (hash) {
-        if (!hash) {
-            return false;
-        }
+const keySection = 'qtranslate-xt-admin-section';
 
-        const anchor = $('.nav-tab-wrapper a[href="' + hash + '"]');
-        if (!anchor.length) {
-            return false;
-        }
+const switchTab = function (hash) {
+    if (!hash) {
+        return false;
+    }
 
-        anchor.parent().children().removeClass('nav-tab-active');
-        anchor.addClass('nav-tab-active');
+    const anchor = $('.nav-tab-wrapper a[href="' + hash + '"]');
+    if (!anchor.length) {
+        return false;
+    }
 
-        const form = $('#qtranxs-configuration-form');
-        const tabId = hash.replace('#', '#tab-');
-        const tabContents = $('.tabs-content');
-        tabContents.children().addClass('hidden');
-        tabContents.find('div' + tabId).removeClass('hidden');
-        const action = form.attr('action').replace(/(#.*|$)/, hash);
-        form.attr('action', action);
+    anchor.parent().children().removeClass('nav-tab-active');
+    anchor.addClass('nav-tab-active');
 
-        try {
-            sessionStorage.setItem(keySection, hash)
-        } catch (e) {
-            // no big deal if this can't be stored
-            console.log('Failed to store "' + keySection + '" with sessionStorage', e);
-        }
+    const form = $('#qtranxs-configuration-form');
+    const tabId = hash.replace('#', '#tab-');
+    const tabContents = $('.tabs-content');
+    tabContents.children().addClass('hidden');
+    tabContents.find('div' + tabId).removeClass('hidden');
+    const action = form.attr('action').replace(/(#.*|$)/, hash);
+    form.attr('action', action);
 
-        return true;
-    };
+    try {
+        sessionStorage.setItem(keySection, hash)
+    } catch (e) {
+        // no big deal if this can't be stored
+        console.log('Failed to store "' + keySection + '" with sessionStorage', e);
+    }
 
-    const onHashChange = function (defaultHash) {
-        const locationHash = window.location.hash;
-        if (switchTab(locationHash)) {
-            return;
-        }
+    return true;
+};
 
-        const storedHash = sessionStorage.getItem(keySection);
-        if (switchTab(storedHash)) {
-            return;
-        }
+const onHashChange = function (defaultHash) {
+    const locationHash = window.location.hash;
+    if (switchTab(locationHash)) {
+        return;
+    }
 
-        switchTab(defaultHash);
-    };
+    const storedHash = sessionStorage.getItem(keySection);
+    if (switchTab(storedHash)) {
+        return;
+    }
 
-    const onFlagChange = function (url) {
-        const $preview = $('#preview_flag');
-        $preview.css('display', 'inline');
-        $preview.attr('src', $preview.attr('data-flag-path') + url);
-    };
+    switchTab(defaultHash);
+};
 
-    $(function () {
-        $(window).bind('hashchange', function () {
-            onHashChange();
-        });
-        onHashChange('#general');
+const onFlagChange = function (url) {
+    const $preview = $('#preview_flag');
+    $preview.css('display', 'inline');
+    $preview.attr('src', $preview.attr('data-flag-path') + url);
+};
 
-        const $langFlag = $('#language_flag');
-        $langFlag.on('change', function () {
-            onFlagChange(this.value);
-        });
-        onFlagChange($langFlag.val());
-
-        $('#qtranxs_debug_query').on('click', function () {
-            const ca = document.cookie.split(';');
-            const browserInfo = {
-                'cookies': [],
-                'navigator': navigator.userAgent
-            };
-            for (let i = 0; i < ca.length; i++) {
-                const cookieStr = ca[i].trim();
-                if (cookieStr.indexOf('qtrans') === 0) {
-                    browserInfo['cookies'].push(cookieStr);
-                }
-            }
-
-            $('#qtranxs_debug_info').show();
-            $('#qtranxs_debug_info_browser').val(JSON.stringify(browserInfo, null, 2));
-            $('#qtranxs_debug_info_versions').val('...');
-            $('#qtranxs_debug_info_configuration').val('...');
-
-            $.ajax({
-                url: ajaxurl,
-                dataType: 'json',
-                data: {
-                    action: 'admin_debug_info'
-                },
-                success: function (response) {
-                    console.log('debug-info', response);
-                    $('#qtranxs_debug_info_versions').val(JSON.stringify(response['versions'], null, 2));
-                    $('#qtranxs_debug_info_configuration').val(JSON.stringify(response['configuration'], null, 2));
-                },
-                error: function (xhr) {
-                    console.error('debug-info', xhr);
-                    $('#qtranxs_debug_info_versions').val('An error occurred: status=' + xhr.status + ' (' + xhr.statusText + ')');
-                }
-            });
-        })
+$(function () {
+    $(window).bind('hashchange', function () {
+        onHashChange();
     });
-})(jQuery);
+    onHashChange('#general');
+
+    const $langFlag = $('#language_flag');
+    $langFlag.on('change', function () {
+        onFlagChange(this.value);
+    });
+    onFlagChange($langFlag.val());
+
+    $('#qtranxs_debug_query').on('click', function () {
+        const ca = document.cookie.split(';');
+        const browserInfo = {
+            'cookies': [],
+            'navigator': navigator.userAgent
+        };
+        for (let i = 0; i < ca.length; i++) {
+            const cookieStr = ca[i].trim();
+            if (cookieStr.indexOf('qtrans') === 0) {
+                browserInfo['cookies'].push(cookieStr);
+            }
+        }
+
+        $('#qtranxs_debug_info').show();
+        $('#qtranxs_debug_info_browser').val(JSON.stringify(browserInfo, null, 2));
+        $('#qtranxs_debug_info_versions').val('...');
+        $('#qtranxs_debug_info_configuration').val('...');
+
+        $.ajax({
+            url: ajaxurl,
+            dataType: 'json',
+            data: {
+                action: 'admin_debug_info'
+            },
+            success: function (response) {
+                console.log('debug-info', response);
+                $('#qtranxs_debug_info_versions').val(JSON.stringify(response['versions'], null, 2));
+                $('#qtranxs_debug_info_configuration').val(JSON.stringify(response['configuration'], null, 2));
+            },
+            error: function (xhr) {
+                console.error('debug-info', xhr);
+                $('#qtranxs_debug_info_versions').val('An error occurred: status=' + xhr.status + ' (' + xhr.statusText + ')');
+            }
+        });
+    })
+});
