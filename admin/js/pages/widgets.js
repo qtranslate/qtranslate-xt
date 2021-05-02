61400ebf3f2392d02ea9f41d712b84f193fe4b1a
HerrVigg <herrvigg@gmail.com>
2021-05-02 14:43:15 +0200

    Draft custom_html

--- admin/js/pages/widgets.js
+++ admin/js/pages/widgets.js
@@ -41,27 +41,61 @@ $(document).on('qtxLoadAdmin:widgets', (event, qtx) => {
         qtx.loadAdditionalTinyMceHooks();
     });
 
-    // TODO hook elements of basic widgets without TinyMCE such as CustomHTM
+    // TODO hook elements of basic widgets without TinyMCE such as CustomHTML
+    const onWidgetAdd = function (evt, widget) {
+        // const $widget = $(widget);
+        const widget_base = widget.find('.id_base').val();
+        console.log('onWidgetAdd', widget, widget_base);
+        switch(widget_base) {
+            case 'custom_html':
+                widget.find(".custom-html-widget-fields input[id$='_title']").each(function (i, e) {
+                    console.log('found title', e)
+                    const ret = qtx.addContentHookById(e.id, '[', 'title');
+                    console.log('addContentHook', ret)
+                    // qtx.refreshContentHook(e);
+                });
+                widget.find(".custom-html-widget-fields textarea[id$='_content']").each(function (i, e) {
+                    console.log('found content', e)
+                    const ret = qtx.addContentHookById(e.id, '[', 'content');
+                    console.log('addContentHook', ret)
+                    // qtx.refreshContentHook(e);
+                });
+                widget.find(".custom-html-widget-fields .CodeMirror-wrap").addClass('qtranxs-translatable');
+                break;
+            case 'text_widget':
+                // Maybe initialize stuff here?
+                break;
+        }
+    }
 
     const onWidgetUpdate = function (evt, widget) {
-        console.log('onWidgetUpdate', widget);
-        widget.find('span.in-widget-title').each(function (i, e) {
-            qtx.addDisplayHook(e);
-        });
-        widget.find("input.qtranxs-translatable").each(function (i, e) {
-            qtx.refreshContentHook(e);
-        });
-        widget.find("textarea.qtranxs-translatable").each(function (i, e) {
-            qtx.refreshContentHook(e);
-        });
+        // const widget = $(widget);
+        const widget_base = widget.find('.id_base').val();
+        console.log('onWidgetUpdate', widget, widget_base);
+        switch(widget_base) {
+            case 'custom_html':
+                // TODO
+                break;
+            case 'text_widget':
+                widget.find('span.in-widget-title').each(function (i, e) {
+                    qtx.refreshContentHook(e);
+                });
+                widget.find("input.qtranxs-translatable").each(function (i, e) {
+                    qtx.refreshContentHook(e);
+                });
+                widget.find("textarea.qtranxs-translatable").each(function (i, e) {
+                    qtx.refreshContentHook(e);
+                });
+                break;
+        }
     };
 
-    // $(document).on('widget-added', onWidgetUpdate);
+    $(document).on('widget-added', onWidgetAdd);
     $(document).on('widget-updated', onWidgetUpdate);
 
     const onLanguageSwitchAfter = function () {
-        console.log('onLanguageSwitchAfter');
         $('#widgets-right .widget').each(function () {
+            console.log('onLanguageSwitchAfter', this);
             wpWidgets.appendTitle(this);
         });
     };
