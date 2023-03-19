c14c2db17689e153d7678f5653a146048c422bc5
Herr Vigg <herrvigg@gmail.com>
2023-03-19 23:57:33 +0100

    Refactor admin CSS and LSB files (#1294)
    The CSS files for LSB should not be processed and embedded every time. This complicates the code and it's not efficient for the cache.
    Load the LSB CSS as files as it's not dependent on embedded code (for user color).
    Enable localization of LSB options.
    Rename the CSS files in lower case separated by hyphens.
    Rename the main `qtranslate_configuration.css` as `admin.css`.
    Tidy up highlight CSS code.

--- src/admin/admin_options_update.php
+++ src/admin/admin_options_update.php
@@ -910,6 +910,10 @@ function qtranxf_update_settings() {
     if ( $_POST['highlight_mode'] != QTX_HIGHLIGHT_MODE_CUSTOM_CSS ) {
         $_POST['highlight_mode_custom_css'] = '';
     }
+    if ( isset( $_POST['lsb_style'] ) && $_POST['lsb_style'] !== QTX_LSB_STYLE_CUSTOM ) {
+        // Admin can only select some option, if CSS file doesn't exist in expected path it's a bug, not a config error.
+        assert( file_exists( QTRANSLATE_DIR . '/css/lsb/' . $_POST['lsb_style'] ) );
+    }
 
     qtranxf_parse_post_type_excluded();
     // special cases handling for admin options - end
