fa95f7e9d633a7b40763840787b113032bd16929
HerrVigg <herrvigg@gmail.com>
2023-03-30 23:52:27 +0200

    Deprecate ACF-qTranslate text/textarea/wysiwyg fields
    Follow-up of #1186 and #1300.
    Since the standard ACF fields are better supported, those should be
    preferred over the qTranslate extended fields.
    
    In particular: text, textarea, wysiwyg are covered by the ACF standard
    fields, while others only exist as qTranslate extended fields.
    
    Deprecate softly these "duplicated" extended fields in their labels.

--- src/modules/acf/fields/textarea.php
+++ src/modules/acf/fields/textarea.php
@@ -28,7 +28,7 @@ class QTX_Module_Acf_Field_Textarea extends acf_field_textarea {
         parent::initialize();
         $this->name     = 'qtranslate_textarea';
         $this->category = QTX_Module_Acf_Register::ACF_CATEGORY_QTX;
-        $this->label    .= ' [' . $this->category . ']';
+        $this->label    .= ' [' . $this->category . ']' . ' - ' . __( 'Deprecated', 'qtranslate' );
     }
 
     /**
