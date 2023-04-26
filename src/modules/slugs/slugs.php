bcd231ef1ccb5a6625bf0a27d7031e5b21459346
HerrVigg <herrvigg@gmail.com>
2023-04-27 00:53:56 +0200

    Add comments and TODOs

--- src/modules/slugs/slugs.php
+++ src/modules/slugs/slugs.php
@@ -326,6 +326,7 @@ class QTX_Module_Slugs {
             }
 
             // If req_uri is empty or if it is a request for ourself, unset error.
+            // TODO: improve string check if admin base is found at other positions in URL.
             if ( empty( $request ) || $req_uri == $self || strpos( $_SERVER['PHP_SELF'], qtranxf_get_admin_base() . '/' ) !== false ) {
                 unset( $_GET['error'] );
                 unset( $error );
