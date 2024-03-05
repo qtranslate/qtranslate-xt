046b8c69809540f9947366255e9d5e1cf9d1e89f
Giovanni Cascione <ing.cascione@gmail.com>
2024-03-05 01:05:47 +0100

    Fix site url redirection when default language is hidden

--- src/modules/slugs/early_hooks.php
+++ src/modules/slugs/early_hooks.php
@@ -3,7 +3,7 @@
 add_filter( 'qtranslate_language_detect_redirect', 'qtranxf_slugs_language_detect_redirect', 600, 3 );
 
 /**
-* Allows url redirection due to language auto detection only if site url has been requested
+* Disables all redirections except when site url is requested, if default language is hidden. In this case url is redirected to default language site url.
 *
 * @see qtranxf_check_url_maybe_redirect
 * @param string $url_lang proposed target URL for the active language to redirect to.
@@ -14,14 +14,23 @@ add_filter( 'qtranslate_language_detect_redirect', 'qtranxf_slugs_language_detec
 */
 function qtranxf_slugs_language_detect_redirect($url_lang, $url_orig, $url_info): string {
    global $q_config;
-   if (site_url().'/' === $url_orig)
-       return $url_lang;
-   else {
-       if ( empty( $url_info['lang_url'] ) ){
-           return qtranxf_convertURL( $url_orig, $q_config['default_language'],false,true );
-       } else {
-           return $url_orig;
-       }
-   }
-}
 
+   if ( ( site_url().'/' === $url_orig && $q_config['hide_default_language'] ) || empty( $url_info['lang_url'] ) ){
+
+      $url_res = qtranxf_convertURL( $url_orig, $q_config['default_language'],false,true );
+
+      if ( qtranxf_detect_language_front( $url_info ) != $q_config['default_language'] ) {
+
+          if ( $url_info['cookie_front_or_admin_found'] )
+            qtranxf_set_language_cookie( $q_config['default_language'] );
+
+          if ( $url_res == $url_orig )
+            $q_config['url_info'] = qtranxf_url_set_language( $urlinfo, $q_config['default_language'], $showLanguage );
+
+      }
+
+   } else
+      $url_res = $url_orig;
+
+   return $url_res;
+}
